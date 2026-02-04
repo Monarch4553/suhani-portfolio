1b5d3e1132bb9d9debc651975fe86225ae3228aa
Monarch <jhondavidson1122@gmail.com>
2026-02-04 16:45:33 +0530

    first commit

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,30 @@
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
+import Rounded from '../../common/RoundedButton';
+import { opacity, slideUp } from './animation';
+import styles from './style.module.scss';
+export default function Description() {
+
+    const phrase = "I work in the sweet spot for innovation, somewhere between strategy, design, and technology. I love the Web and the work we do.";
+    const description = useRef(null);
+    const isInView = useInView(description)
+    return (
+        <div ref={description} className={styles.description}>
+            <div className={styles.body}>
+                <p>
+                    {
+                        phrase.split(" ").map((word, index) => {
+                            return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
+                        })
+                    }
+                </p>
+                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>I am a passionate theme developer with expertise in designing and developing innovative web experiences. I specialize in Front-End Development, Theme Customization, Responsive Design, and Performance Optimization.</motion.p>
+                <div data-scroll data-scroll-speed={0.1}>
+                    <Rounded className={styles.button}>
+                        <p>About me</p>
+                    </Rounded>
+                </div>
+            </div>
+        </div>
+    )
+}
